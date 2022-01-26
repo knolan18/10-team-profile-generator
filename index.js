a5d42d5dce2af55588e633d7669b33a3165bb2cc
knolan18 <minicreature@Kellys-Mac-mini.local>
2022-01-26 09:13:23 -0800

    draft files before testing

---
+++ index.js
@@ -0,0 +1,130 @@
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path')
+const DIST_Dir = path.resolve(__dirname, 'dist');
+const distPath = path.join(DIST_Dir, 'team.html')
+
+const generateHtml = require('./util/generateHtml');
+const employees = [];
+
+function start() {
+  // makeManager function
+  function makeManager() {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'managerName',
+        message: 'What is the name of the manager?'
+      },
+      {
+        type: 'input',
+        name: 'managerId',
+        message: 'What is the id of the manager?'
+      },
+      {
+        type: 'input',
+        name: 'managerEmail',
+        message: 'What is the email of the manager?'
+      },
+      {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'What is the office number of the manager?'
+      },
+    ])
+    .then(res => {
+      const manager = new Manager(res.managerName, res.managerId, res.managerEmail, res.officeNumber)
+      employees.push(manager);
+      cycleQuestion()
+    })
+  }
+
+  // makeEngineer function
+  function makeEngineer() {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'engineerName',
+        message: 'What is the name of the engineer?'
+      },
+      {
+        type: 'input',
+        name: 'engineerId',
+        message: 'What is the id of the engineer?'
+      },
+      {
+        type: 'input',
+        name: 'engineerEmail',
+        message: 'What is the email of the engineer?'
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'What is the GitHub username of the engineer?'
+      },
+    ])
+  }  
+  // makeIntern function
+  function makeManager() {
+    inquirer.prompt([
+      {
+        type: 'input',
+        name: 'internName',
+        message: 'What is the name of the intern?'
+      },
+      {
+        type: 'input',
+        name: 'internId',
+        message: 'What is the id of the intern?'
+      },
+      {
+        type: 'input',
+        name: 'internEmail',
+        message: 'What is the email of the intern?'
+      },
+      {
+        type: 'input',
+        name: 'school',
+        message: 'What is the school of the intern?'
+      },
+    ])
+  }
+  // buildHtml function goes here (Use readme generator fs function as reference)
+  function buildHtml() {
+    if(!fs.existsSync(DIST_Dir)) {
+      fs.mkdirSync(DIST_Dir);
+    }
+
+    fs.writeFileSync(distPath, generateHtml(employees), 'utf-8')
+  }
+
+  function cycleQuestion() {
+    inquirer.prompt([
+      {
+        type: 'list',
+        name: 'teamMember',
+        message: 'What team member would you like to make next?',
+        choices: ['Engineer', 'Intern', 'Done']
+      }
+    ])
+    .then(res => {
+      switch(res.teamMember){
+        case 'Engineer':
+          makeEngineer();
+          break;
+        case 'Intern':
+          makeIntern();
+          break;
+        default:
+          buildHtml();
+      }
+    })
+  }
+
+  makeManager();
+}
+
+start();
